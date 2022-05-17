9fae5c8cdeb1d9299d04170dde1a57d54dedf83b
Muhamad Iqbal <muhamad_iqbal23@outlook.com>
2022-05-17 10:47:25 +0800

    Initial Commit with product

---
+++ database/migrations/2022_05_17_022405_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price')->comment('in sen');
+            $table->text('desc_short')->nullable();
+            $table->text('desc_long')->nullable();
+            $table->text('slug');
+            $table->text('keywords')->nullable();
+            $table->string('picture');
+            $table->string('picture_2')->nullable();
+            $table->string('picture_3')->nullable();
+            $table->string('picture_4')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('product_categories');
+            $table->integer('weight')->comment('in gram')->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
